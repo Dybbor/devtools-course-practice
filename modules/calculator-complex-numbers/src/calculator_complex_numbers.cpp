817f448826716c161626ddbada076fb5f3b75dea
Dybbor <19dobrohotov99@mail.ru>
2020-04-08 12:55:53 +0300

    add starts file, cmake build succeed

---
+++ modules/calculator-complex-numbers/src/calculator_complex_numbers.cpp
@@ -0,0 +1,6 @@
+#include "include/calculator_complex_numbers.h" 
+
+CalculatorComplexNumbers::CalculatorComplexNumbers()
+{
+	this->a = 0;
+}
---
+++ modules/calculator-complex-numbers/test/test_calculator_complex_numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <gtest/gtest.h>
+
+#include "include/calculator_complex_numbers.h"
+
+TEST(CalculatorComplexNumbers, test)
+{
+    std::cout << "All work" << std::endl;
+    EXPECT_EQ(1, 1);
+}
