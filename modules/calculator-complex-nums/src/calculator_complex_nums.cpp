24050afb3e123210df424b46c114e4c3289aab52
Vitaly Dobrohotov <19dobrohotov99@mail.ru>
2020-04-08 18:57:13 +0300

    and another fix for trevis

--- modules/calculator-complex-nums/src/calculator_complex_nums.cpp
+++ modules/calculator-complex-nums/src/calculator_complex_nums.cpp
@@ -1,11 +1,14 @@
+// Copyright 2020 Dobrohotov Vitaly
+
 #include <include/calculator_complex_nums.h>
+
+#include <string>
 
 CalculatorComplexNums::CalculatorComplexNums() : pattern("") {}
 
 CalculatorComplexNums::CalculatorComplexNums(std::string _pattern) :
     pattern(_pattern) {}
 
-
 std::string CalculatorComplexNums::GetPattern() {
-	return pattern;
+    return pattern;
 }
