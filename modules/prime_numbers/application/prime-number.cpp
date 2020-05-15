e2bc76077e65d485281bb09829fa295386e79e09
Vitaly Dobrohotov <19dobrohotov99@mail.ru>
2020-05-15 17:56:13 +0300

    add application and starts file

---
+++ modules/prime_numbers/application/prime-number.cpp
@@ -0,0 +1,11 @@
+// Copyright 2020 Dobrohotov Vitaly
+
+#include <iostream>
+#include <string>
+
+#include "include/TPrime_Nums.h"
+
+int main(int argc, const char** argv) {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
